f3bee46e9530b3807b7698023a190c8eca35de4c
Dmytro Trifonov <dmytro.trifonov@gmail.com>
2016-07-03 13:55:29 +0300

    Added React Router configuration

--- app/client.js
+++ app/client.js
@@ -1,6 +1,12 @@
 import 'react-toolbox/lib/commons.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { browserHistory, Router, Route } from 'react-router';
 import App from './components/App.js';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/user-dashboard" component={App}>
+    </Route>
+  </Router>
+), document.getElementById('app'));
